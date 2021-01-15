35c06dfce65ef48bf4e3f64dc68bf37c9dc7f550
Mikhael <mikhaelapg00@gmail.com>
2021-01-15 22:32:26 +0700

    assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-    <h1>About Me</h1>
-    <p>Hallo, Nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+    <div class="container">
+        <h1 class="mt-4">About Me</h1>
+        <img src="<?= BASEURL; ?>/img/me.jpg" alt="Me" width="200" class="rounded-circle shadow">
+        <p>Hallo, Nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+    </div>
