5f38a25f35449824a9f35772d4c4a93abfd291ec
Mikhael <mikhaelapg00@gmail.com>
2021-01-19 17:21:03 +0700

    update data

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -8,7 +8,7 @@
 
     <div class="row">
         <div class="col-lg-6">
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+            <button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModal">
                 Tambah Data Mahasiswa
             </button>
             <br><br>
@@ -18,6 +18,7 @@
                         <li class="list-group-item">
                             <?= $mhs['nama']; ?>
                             <a href="<?=  BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge badge-pill badge-danger float-right ml-1" onclick="return confirm('yakin?');">hapus</a>
+                            <a href="<?=  BASEURL; ?>/mahasiswa/ubah/<?= $mhs['id']; ?>" class="badge badge-pill badge-success float-right ml-1 tampilModalUbah" data-toggle="modal" data-target="#formModal" data-id="<?= $mhs['id']; ?>">edit</a>
                             <a href="<?=  BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-pill badge-primary float-right ml-1">detail</a>
                         </li>
                     <?php endforeach; ?>
@@ -38,6 +39,7 @@
       </div>
         <div class="modal-body">
         <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+            <input type="hidden" name="id" id="id">
             <div class="form-group">
                 <label for="nama">Nama</label>
                 <input type="text" class="form-control" id="nama" name="nama">
---
+++ public/js/script.js
@@ -0,0 +1,34 @@
+$(function() {
+
+    $('tombolTambahData').on('click', function() {
+
+        $('#judlModal').html('Tambah Data Mahasiswa');
+        $('.modal-footer button[type=submit]').html('Tambah Data');
+
+    });
+
+    $('.tampilModalUbah').on('click', function() {
+
+        $('#judulModal').html('Ubah Data Mahasiswa');
+        $('.modal-footer button[type=submit]').html('Ubah Data');
+        $('.modal-body form').attr('action', 'http://localhost/phpmvc/public/mahasiswa/ubah');
+
+        const id = $(this).data('id');
+
+        $.ajax({
+            url: 'http://localhost/phpMVC/public/mahasiswa/getubah',
+            data: {id : id},
+            method: 'post',
+            dataType: 'json',
+            success: function(data) {
+                $('#nama').val(data.nama);
+                $('#nrp').val(data.nrp);
+                $('#email').val(data.email);
+                $('#jurusan').val(data.jurusan);
+                $('#id').val(data.id);
+            }
+        });
+        
+    });
+
+});
